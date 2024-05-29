8f94bdb54008442c35a44014322b47679332ad84
PedroNavarro <phvnavarro@gmail.com>
2024-05-29 19:23:47 -0300

    added parameters to user bp

--- controllers/users_controllers.py
+++ controllers/users_controllers.py
@@ -1,4 +1,4 @@
 from flask import Blueprint, request, render_template, redirect, url_for
 from models.user.user import User
 
-users_ = Blueprint()
+users_ = Blueprint("users_",__name__, template_folder="views")
