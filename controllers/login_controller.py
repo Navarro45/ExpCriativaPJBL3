94c6afd5d97aafca4572c6acad322bf998b7b8c5
PedroNavarro <phvnavarro@gmail.com>
2024-05-31 19:37:03 -0300

    corrected login functions

--- controllers/login_controller.py
+++ controllers/login_controller.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, request, render_template, redirect, url_for
+from flask import Blueprint, request, render_template, redirect, url_for, flash
 from models.user.user import User
 import flask_login
 
@@ -9,18 +9,20 @@ login_ = Blueprint("login_",__name__, template_folder="views")
 def index():
     return render_template('login.html')
 
-@login_.route('/validated_user', methods=['POST'])
+@login_.route('/validated_user', methods=['POST','GET'])
 def validated_user():
     if request.method == 'POST':
-        user = request.form['user']
+        username = request.form['username']
         password = request.form['password']
-        users = User.get_users()
-        if user in users and users[user] == password:
-            user_ = User.get_users()
-            user_.id = user
-            flask_login.login_user(user_)
+        
+        # Buscar usuário pelo nome de usuário
+        user = User.query.filter_by(username=username).first()
+        
+        if user and user.password == password:  # Comparar diretamente a senha sem hash (não recomendado)
+            flask_login.login_user(user)
             return render_template('home.html')
         else:
-            return '<h1>invalid credentials!</h1>'
+            flash('Invalid credentials!')
+            return '<h1>Invalid credentials!</h1>'
     else:
         return render_template('login.html')
--- utils/create_db.py
+++ utils/create_db.py
@@ -4,5 +4,4 @@ from models.db import db
 
 def create_db(app:Flask):
     with app.app_context():
-        db.drop_all()
         db.create_all()
