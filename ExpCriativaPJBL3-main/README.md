8f6f8bfdb10658118ea28c0ebf3a38919bb9a1e3
PedroNavarro <phvnavarro@gmail.com>
2024-05-31 14:10:17 -0300

    Deleted extra files

--- ExpCriativaPJBL3-main/README.md
+++
@@ -1,1 +0,0 @@
-# ExpCriativaPJBL3
--- ExpCriativaPJBL3-main/models/__init__.py
+++
@@ -1,5 +0,0 @@
-from models.db import db
-from models.iot.devices import Device
-from models.iot.sensors import Sensor
-from models.iot.actuators import Actuator
-from models.iot.read import Read
