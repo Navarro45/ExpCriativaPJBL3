36e264bd176bbf315dac53d39eff74e49c22c0b0
arturp <arturpmeneghete@gmail.com>
2024-06-10 23:12:05 -0300

    actuator

--- controllers/actuators_controller.py
+++ controllers/actuators_controller.py
@@ -16,7 +16,6 @@ def add_actuator():
     topic = request.form.get("topic")
     unit = request.form.get("unit")
     is_active = True if request.form.get("is_active") == "on" else False
-    
     Actuator.save_actuator(name, topic, unit, is_active)
     return redirect(url_for('actuators_.actuators'))
 
@@ -25,7 +24,7 @@ def add_actuator():
 def edit_actuator():
     id = request.args.get('id', None)
     actuator = Actuator.get_single_actuator(id)
-    return render_template("update_actuator.html", actuator=actuator)
+    return render_template("update_actuator.html", actuator=actuator[0])
 
 @actuators_.route('/update_actuator', methods=['POST'])
 @login_required
@@ -35,7 +34,7 @@ def update_actuator():
     topic = request.form.get("topic")
     unit = request.form.get("unit")
     is_active = True if request.form.get("is_active") == "on" else False
-    Actuator.update_actuator(id, name, topic, unit, is_active)
+    actuator = Actuator.update_actuator(id, name, topic, unit, is_active)
     return redirect(url_for('actuators_.actuators'))
 
 @actuators_.route('/del_actuator', methods=['GET'])
@@ -49,8 +48,3 @@ def del_actuator():
 @login_required
 def actuators():
     return render_template("actuators.html", devices=Actuator.get_actuators())
-
-@actuators_.route('/actuatorsuser')
-@login_required
-def actuatorsuser():
-    return render_template("actuatorsuser.html", devices=Actuator.get_actuators())
--- models/iot/actuators.py
+++ models/iot/actuators.py
@@ -5,6 +5,7 @@ class Actuator(db.Model):
     __tablename__ = 'actuators'
     id = db.Column('id', db.Integer, primary_key=True)
     devices_id = db.Column(db.Integer, db.ForeignKey(Device.id))
+    name = db.Column(db.String(50))  # Adicionando a coluna 'name'
     unit = db.Column(db.String(50))
     topic = db.Column(db.String(50))
 
@@ -12,15 +13,15 @@ class Actuator(db.Model):
     def save_actuator(name, topic, unit, is_active):
         device = Device(name=name, is_active=is_active)
         db.session.add(device)
-        db.session.commit()  # Commit here to get the device ID
-        actuator = Actuator(devices_id=device.id, unit=unit, topic=topic)
+        db.session.commit()  # Commit aqui para obter o ID do dispositivo
+        actuator = Actuator(devices_id=device.id, name=name, unit=unit, topic=topic)
         db.session.add(actuator)
         db.session.commit()
 
     @staticmethod
     def get_actuators():
         actuators = Actuator.query.join(Device, Device.id == Actuator.devices_id)\
-            .add_columns(Device.id, Device.name, Device.is_active, Actuator.topic, Actuator.unit).all()
+            .add_columns(Device.id, Device.name, Device.is_active, Actuator.topic, Actuator.unit, Actuator.name).all()
         return actuators
 
     @staticmethod
@@ -28,21 +29,31 @@ class Actuator(db.Model):
         actuator = Actuator.query.filter(Actuator.devices_id == id).first()
         if actuator:
             actuator = Actuator.query.filter(Actuator.devices_id == id)\
-                .join(Device).add_columns(Device.id, Device.name, Device.is_active, Actuator.topic, Actuator.unit).first()
+                .join(Device).add_columns(Device.id, Device.name, Device.is_active, Actuator.topic, Actuator.unit, Actuator.name).first()
             return [actuator]
 
     @staticmethod
     def update_actuator(id, name, topic, unit, is_active):
-        device = Device.query.filter(Device.id == id).first()
-        actuator = Actuator.query.filter(Actuator.devices_id == id).first()
-        if device:
-            device.name = name
-            device.is_active = is_active
-            db.session.commit()
-        if actuator:
-            actuator.topic = topic
-            actuator.unit = unit
-            db.session.commit()
+        try:
+            device = Device.query.filter(Device.id == id).first()
+            actuator = Actuator.query.filter(Actuator.devices_id == id).first()
+
+            if device:
+                print(f"Updating device: {device.id}")
+                device.name = name
+                device.is_active = is_active
+                db.session.commit()
+
+            if actuator:
+                print(f"Updating actuator: {actuator.id}")
+                actuator.name = name  # Atualizando o nome no atuador
+                actuator.topic = topic
+                actuator.unit = unit
+                db.session.commit()
+
+        except Exception as e:
+            print(f"Error updating actuator: {e}")
+
         return Actuator.get_actuators()
 
     @staticmethod
